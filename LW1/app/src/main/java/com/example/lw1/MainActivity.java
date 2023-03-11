1e2732d4c72483efba61b2a49eadaf4855be4532
Kirill Budko <budkokira@vk.com>
2023-03-11 11:41:04 +0300

    01 / Описан класс DrawView

--- LW1/app/src/main/java/com/example/lw1/MainActivity.java
+++ LW1/app/src/main/java/com/example/lw1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.lw1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.content.Context;
+import android.view.View;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    class DrawView extends View {
+        Paint paint;
+        Rect rect;
+
+        public DrawView(Context context) {
+            super(context);
+            paint = new Paint();
+            rect = new Rect();
+        }
     }
 }
