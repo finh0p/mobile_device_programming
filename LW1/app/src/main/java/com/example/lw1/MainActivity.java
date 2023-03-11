1342e5833cc45afbf479792d0a85744ba309b7f6
Kirill Budko <budkokira@vk.com>
2023-03-11 13:36:00 +0300

    01 / Выполнены все шаги, описанные в ходе лабораторной работы

--- LW1/app/src/main/java/com/example/lw1/MainActivity.java
+++ LW1/app/src/main/java/com/example/lw1/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.lw1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.content.Context;
+import android.graphics.RectF;
 import android.view.View;
 import android.os.Bundle;
 
@@ -14,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new DrawView(this));
+        setTitle("Test Task");
     }
 
     class DrawView extends View {
@@ -30,6 +34,21 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onDraw(Canvas canvas) {
             canvas.drawARGB(80, 102, 104, 255);
+            paint.setColor(Color.RED);
+            canvas.drawRect(200, 105, 400, 200, paint);
+            canvas.drawCircle(100, 200, 50, paint);
+            paint.setStrokeWidth(10);
+            float[] points = new float[] {300, 200, 400, 200};
+            canvas.drawLines(points, paint);
+            Path path = new Path();
+            path.moveTo(50, 50);
+            path.lineTo(0, 100);
+            path.lineTo(100, 100);
+            path.lineTo(50, 50);
+            canvas.drawPath(path, paint);
+            Path path2 = new Path();
+            path2.addArc(new RectF(0, 0, 100, 100), 90, 180);
+            canvas.drawPath(path2, paint);
         }
     }
 }
