7862d5788fc2d196fc07a308fdbfc2b2552f213b
Kirill Budko <budkokira@vk.com>
2023-03-26 01:09:05 +0300

    01 / Создан круг, касающийся всех граней экрана

--- LW1/app/src/main/java/com/example/lw1/MainActivity.java
+++ LW1/app/src/main/java/com/example/lw1/MainActivity.java
@@ -1,24 +1,40 @@
 package com.example.lw1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.content.Context;
 import android.graphics.RectF;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new DrawView(this));
-        setTitle("Test Task");
+        TextView textView = new TextView(this);
+//        textView.setText(R.string.TextView);
+//        textView.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+        setTitle("Kirill Budko");
+
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+        constraintLayout.addView(textView);
+//        constraintLayout.addView(new DrawView(this));
+//        setContentView(constraintLayout);
+//        setContentView(new DrawView(this));
+        constraintLayout.addView(new MyDrawView(this));
+        setContentView(constraintLayout);
     }
 
     class DrawView extends View {
@@ -49,6 +65,31 @@ public class MainActivity extends AppCompatActivity {
             Path path2 = new Path();
             path2.addArc(new RectF(0, 0, 100, 100), 90, 180);
             canvas.drawPath(path2, paint);
+        }
+    }
+
+    class MyDrawView extends View {
+
+        Paint paint;
+        Rect rect;
+
+        public MyDrawView(Context context){
+            super(context);
+            paint = new Paint();
+            rect = new Rect();
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            paint.setColor(Color.RED);
+            paint.setStrokeWidth(10);
+            paint.setAlpha(128);
+            DisplayMetrics metrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+            float height = metrics.heightPixels * 0.95f;
+            float width = metrics.widthPixels;
+            canvas.drawOval(0.0f, 0.0f, width, height, this.paint);
         }
     }
 }
